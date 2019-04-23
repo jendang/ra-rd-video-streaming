76d518c4249eafd0978b7e1437ba06175dbdd1b6
jennydang <jenny.dang117@gmail.com>
2019-04-23 22:20:11 +0200

    header and inside links done

--- stream-client/src/App.js
+++ stream-client/src/App.js
@@ -5,12 +5,14 @@ import StreamCreate from './components/streams/StreamCreate';
 import StreamEdit from './components/streams/StreamEdit';
 import StreamDelete from './components/streams/StreamDelete';
 import StreamShow from './components/streams/StreamShow';
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList}/>
           <Route path="/streams/new" exact component={StreamCreate}/>
           <Route path="/streams/edit" exact component={StreamEdit}/>
