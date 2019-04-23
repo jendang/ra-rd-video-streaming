d120fb683103f0410c1bcc4a37a1a61a2c18f063
jennydang <jenny.dang117@gmail.com>
2019-04-23 22:06:16 +0200

    components scaffolding, setting up routes

--- stream-client/src/App.js
+++ stream-client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
+import StreamList from './components/streams/StreamList';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamShow from './components/streams/StreamShow';
 
 function App() {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList}/>
+          <Route path="/streams/new" exact component={StreamCreate}/>
+          <Route path="/streams/edit" exact component={StreamEdit}/>
+          <Route path="/streams/delete" exact component={StreamDelete}/>
+          <Route path="/streams/show" exact component={StreamShow}/>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
