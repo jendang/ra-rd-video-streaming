ba8089288f7f55839817381d3c67f4678af3c289
jennydang <jenny.dang117@gmail.com>
2019-04-27 23:52:38 +0200

    editting stream done, reuse code for form

--- stream-client/src/components/streams/StreamEdit.js
+++ stream-client/src/components/streams/StreamEdit.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
+import _ from 'lodash'
 
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    )
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+       // console.log(this.props.match.params.id)
+    }
+
+    onSubmit = (formValues) => {
+        //console.log(formValues) //formValues is from redux-form
+        this.props.editStream(this.props.match.params.id, formValues)
+    }
+
+    render() {
+        if(!this.props.stream) return <div>Loading</div>
+        return (
+            <div>
+                <h3>Edit a stream</h3>
+                <StreamForm 
+                    onSubmit={this.onSubmit}
+                    // initialValues is from redux-form
+                    //initialValues={{ title: this.props.stream.title, description: this.props.stream.description}}
+                    initialValues={_.pick(this.props.stream, 'title', 'description')}
+                />
+            </div>
+        )
+
+    }
+
+    
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps ) => {
+    //console.log(ownProps)
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+export default connect(mapStateToProps, { fetchStream, editStream }) (StreamEdit)
---
+++ stream-client/src/components/streams/StreamForm.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+class StreamForm extends React.Component {
+    renderError({ error, touched }){
+        if(touched && error){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+    // renderInput(formProps) {
+        //const {input} = formProps.input
+    renderInput = ({ input, label, meta }) => { 
+        //console.log(meta) // show validate() obj that include error's message
+        const className =  `field ${meta.error && meta.touched ? 'error' : ''}`
+        return (
+            // className="field error"
+            <div className={className}> 
+                <label>{label}</label>
+                {/* <input {...formProps.input} /> */}
+                {/* {...input} same like formProps.input.onChange/value/... */}
+                {/* autoComplete off so its not auto fill data */}
+                <input {...input} autoComplete="off" />
+                {/* display error under input field */}
+                {/* <div>{meta.error}</div>  */}
+                <div>{this.renderError(meta)}</div>
+            </div>
+        )
+    }
+
+    onSubmit = (formValues)=> {
+        //console.log(formValues) // when not use redux-form
+        this.props.onSubmit(formValues) //dispatch action from redux- action creators
+    }
+
+    render(){
+        //console.log(this.props) // all props that redux-form support
+        return (
+            //handleSubmit() from this.props support by redux
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field 
+                    name="title" 
+                    component={this.renderInput} 
+                    label="Enter title"
+                />
+                <Field 
+                    name="description" 
+                    component={this.renderInput} 
+                    label="Enter description"
+                />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {}
+    if(!formValues.title) {
+        // only ran if the user did not enter the title
+        errors.title = "You must enter the title"
+    }
+
+    if(!formValues.description) {
+        errors.description = "You must enter the description"
+    }
+
+    return errors
+}
+
+export default reduxForm({
+    form: 'streamForm',
+    validate
+})(StreamForm)
+
+ 
